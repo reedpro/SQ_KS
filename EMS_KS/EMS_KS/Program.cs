210f8ba3992dc6d95dc9cd9d6b93888f868d9c9e
reed_pro <poirier.jordan@gmail.com>
2014-10-09 10:29:33 -0400

    Created Solution for term project

---
+++ EMS_KS/EMS_KS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS_KS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
